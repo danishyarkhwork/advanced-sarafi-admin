72d404d10aebf7f17ef0a1af1d21409961c70e7e
danishyarkhwork <danishyarkhwork@gmail.com>
2024-10-19 09:14:42 +0430

    add: currency context page creted

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-// App.js
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Sidebar from "./components/Sidebar";
-import Dashboard from "./components/Dashboard";
-import ExchangeRates from "./pages/ExchangeRates";
-import AddCurrencyModal from "./components/AddCurrencyModal";
+// src/App.js
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Sidebar from './components/Sidebar';
+import Dashboard from './components/Dashboard';
+import ExchangeRates from './pages/ExchangeRates';
+import AddCurrencyModal from './components/AddCurrencyModal';
+import Login from './pages/Login';
+import Home from './pages/Home';
 
 function App() {
   return (
@@ -12,9 +14,11 @@ function App() {
         <Sidebar />
         <div className="p-4 w-full">
           <Routes>
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/exchange-rates" element={<ExchangeRates />} />
-            <Route path="/add-rate" element={<AddCurrencyModal />} />
+            <Route path="/" element={<Home />} /> {/* Home redirects based on authentication */}
+            <Route path="/login" element={<Login />} /> {/* Login Page */}
+            <Route path="/dashboard" element={<Dashboard />} /> {/* Dashboard Page */}
+            <Route path="/exchange-rates" element={<ExchangeRates />} /> {/* Exchange Rates Management */}
+            <Route path="/add-rate" element={<AddCurrencyModal />} /> {/* Modal to Add Rates */}
           </Routes>
         </div>
       </div>
